457303f1f06fbd265b8aceebca926fcc0cd4c4ec
zarocknc <sebastian_nicolaschong@protonmail.com>
2023-03-04 21:56:11 -0500

    commit

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -2,33 +2,36 @@
 <html lang="en" data-theme="dark">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-    @livewireStyles
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  @vite('resources/css/app.css')
+  @livewireStyles
 </head>
 
 <body>
-    <h1 class="text-3xl underline">Categorias</h1>
+  <h1 class="text-3xl underline">Categorias</h1>
 
-    @livewire('show-categorias')
+  <form action="{{ route('categorias.store') }}" method="POST">
+    @csrf
+    <input class="input input-bordered" type="text" name="categoria" placeholder="Nombre de la Categoría" required>
+    <button type="submit" class="btn btn-primary">Agregar</button>
+  </form>
+  <br>
+
+
+  @livewire('show-categorias')
 
 
 
-    @isset($added)
-        <p> Se ha agregado una nueva categoria </p>
-    @endisset
-    <form action="{{ route('categorias.store') }}" method="POST">
-        @csrf
-        <input class="input input-bordered" type="text" name="categoria" placeholder="Nombre de la Categoría"
-            required>
-        <button type="submit" class="btn btn-primary">Agregar</button>
-    </form>
-    <br>
-<br>
-    @livewireScripts
+  @isset($added)
+  <p> Se ha agregado una nueva categoria </p>
+  @endisset
+
+  <br>
+  <br>
+  @livewireScripts
 </body>
 
 
